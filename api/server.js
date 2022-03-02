82f0fc661078e8358b6a4ab4bd8074a3df23b765
Allison Stewart <amstewart@yahoo.com>
2022-03-02 13:59:14 -0500

    added index and server

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const recipesRouter = require('./recipes/recipes-router');
+const server = express();
+
+server.use(express.json())
+server.use('/api/recipes', recipesRouter)
+
+server.use((err, req, res, next) => { //eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack
+  })
+})
+
+server.use('*', (req, res) => {
+  res.json({ message: 'something went wrong!'})
+})
+
+module.exports = server;
